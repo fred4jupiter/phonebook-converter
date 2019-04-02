c7905e5abd9df50055f449d7ad61682cac4d5294
fred4jupiter <hamsterhase@gmx.de>
2019-04-02 16:12:31 +0200

    invoking shell command

--- src/main/java/de/fred4jupiter/phonebook/converter/ShellCommands.java
+++ src/main/java/de/fred4jupiter/phonebook/converter/ShellCommands.java
@@ -12,6 +12,13 @@ public class ShellCommands {
     @Autowired
     private ExcelToFritzboxXmlService excelToFritzboxXmlService;
 
+    @ShellMethod("Creates an Excel template file.")
+    public String createExcelTemplate(@ShellOption(defaultValue = "template.xlsx") String outputFile) {
+        excelToFritzboxXmlService.createExcelTemplateFile(outputFile);
+
+        return "Created Excel template file: " + outputFile;
+    }
+
     @ShellMethod("Converts a given Excel file to Fritzbox phonebook xml file.")
     public String convertToFritzboxPhonebook(@ShellOption String inputFile, @ShellOption(defaultValue = "phonebook.xml") String outputFile) {
         excelToFritzboxXmlService.readExcelFileAndConvert(inputFile, outputFile);
--- src/main/java/de/fred4jupiter/phonebook/converter/service/ExcelToFritzboxXmlService.java
+++ src/main/java/de/fred4jupiter/phonebook/converter/service/ExcelToFritzboxXmlService.java
@@ -5,23 +5,34 @@ import de.fred4jupiter.phonebook.converter.excel.ExcelImportService;
 import de.fred4jupiter.phonebook.converter.fritzbox.PhonebookBuilder;
 import de.fred4jupiter.phonebook.converter.fritzbox.Phonebooks;
 import de.fred4jupiter.phonebook.converter.xml.XmlCreatorService;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class ExcelToFritzboxXmlService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExcelToFritzboxXmlService.class);
 
     @Autowired
     private ExcelImportService excelImportService;
 
     @Autowired
     private XmlCreatorService xmlCreatorService;
+
+    @Value("classpath:/template/phonebook_template.xlsx")
+    private Resource excelTemplateFile;
 
     public void readExcelFileAndConvert(String excelIn, String xmlOut) {
         List<ExcelContact> excelContacts = excelImportService.importFromExcel(new File(excelIn), this::mapToExcelContact);
@@ -68,5 +79,13 @@ public class ExcelToFritzboxXmlService {
     private String getNumericFieldAsString(Row row, int index) {
         DataFormatter formatter = new DataFormatter();
         return formatter.formatCellValue(row.getCell(index));
+    }
+
+    public void createExcelTemplateFile(String outputFile) {
+        try {
+            FileUtils.copyInputStreamToFile(excelTemplateFile.getInputStream(), new File(outputFile));
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+        }
     }
 }
